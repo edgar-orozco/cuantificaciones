de3fa38181eba29ec75601bf32d956f6a7e21294
edgar-orozco <9440039+edgar-orozco@users.noreply.github.com>
2022-09-09 11:18:23 -0500

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VendorName\Skeleton\Tests;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_test()
+    {
+        $this->assertTrue(true);
+    }
+}
